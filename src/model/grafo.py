3da089cf6060ac83dfefa12e63618da26b6860e5
Jhonkyyy <jhonfre0212@gmail.com>
2025-05-20 22:08:05 -0500

    metodos de añadir estacion y añadir ruta

--- src/model/grafo.py
+++ src/model/grafo.py
@@ -4,4 +4,20 @@ from src.model.ruta import Ruta
 
 class Grafo:
     def __init__(self):
-        self.adjlist: dict[Estacion, Ruta] = {}
+        self.adjlist: dict[Estacion, list[Ruta]] = {}
+
+    def añadir_estacion(self, estacion: Estacion):
+        self.adjlist[estacion] = []
+
+    def añadir_ruta(self, ruta: Ruta):
+        if ruta.origen not in self.adjlist:
+            raise Exception (f"No hay estacion {ruta.origen} en el grafo")
+
+        if ruta.dest not in self.adjlist:
+            raise Exception ("No se encentra estacion destino")
+
+        if ruta in self.adjlist[ruta.origen]:
+            raise Exception ("Ruta ya se encuentra en el grafo")
+
+        else:
+            self.adjlist[ruta.origen].append(ruta)
