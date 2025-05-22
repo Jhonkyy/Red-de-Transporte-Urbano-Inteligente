36147aa4b05b6b81db6eae62e23a91fca8442cc5
Jhonkyyy <jhonfre0212@gmail.com>
2025-05-22 09:39:38 -0500

    peso predeterminado = 1

--- src/model/ruta.py
+++ src/model/ruta.py
@@ -2,7 +2,7 @@ from src.model.estacion import Estacion
 
 
 class Ruta:
-    def __init__(self, origen: Estacion, dest: Estacion, peso: float):
+    def __init__(self, origen: Estacion, dest: Estacion, peso: float = 1):
         self.origen = origen
         self.dest = dest
         self.peso = peso
